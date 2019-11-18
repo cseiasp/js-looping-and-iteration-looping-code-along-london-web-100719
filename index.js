29cd0cc84d55f9587974edb37777c4fa6d865b31
cseiasp <caroline.seilern@hotmail.com>
2019-11-18 14:12:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+import { endianness } from "os";
+
 // Code your solutions in this file
+function writeCards(cards, name){
+     let array = []
+    for (let i = 0; i < cards.length; i++){
+        array.push(`Thank you, ${cards[i]}, for the wonderful ${name} gift!`)
+    }
+
+    return array
+}
+
+function countDown(integer) {
+    while (integer > 0){
+        console.log(integer)
+        integer -= 1
+    }
+    console.log(integer)
+}
+
